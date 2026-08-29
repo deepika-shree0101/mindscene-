00310063d96e2cdfdb994ccc9bcca4dcc0693593
SANJAY-SPK <sanjaysivakumar2261@gmail.com>
2026-08-29 09:10:33 +0530

    Security config added

--- service_backend/src/main/java/com/mysterygame/servicebackend/security/SecurityConfig.java
+++ service_backend/src/main/java/com/mysterygame/servicebackend/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "https://delicate-sfogliatella-21e199.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
         configuration.setAllowCredentials(true);
